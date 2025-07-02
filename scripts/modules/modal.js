198e47d9100b629420948790cda810662f3073fe
Team Cardinalis <killianott4@gmail.com>
2025-07-02 17:36:02 +0200

    Update

--- scripts/modules/modal.js
+++ scripts/modules/modal.js
@@ -42,7 +42,7 @@ export class ModalManager {
             details = null,
             highlight = null,
             prompt = null, // New: for input fields
-            type = 'warning', // 'warning' or 'danger' or 'info'
+            type = 'warning', // 'warning' or 'danger'
             confirmText = 'Confirm',
             cancelText = 'Cancel'
         } = config;
@@ -50,10 +50,7 @@ export class ModalManager {
         const modal = document.createElement('div');
         modal.className = 'modal-overlay';
 
-        let iconSvg = `<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20"><path fill="#ffd500" d="M8.681 2.785c.568-1.047 2.071-1.047 2.638 0l6.5 12.002A1.5 1.5 0 0 1 16.502 17H3.498a1.5 1.5 0 0 1-1.319-2.215zM10.5 7.5a.5.5 0 0 0-1 0v4a.5.5 0 0 0 1 0zm.25 6.25a.75.75 0 1 0-1.5 0a.75.75 0 0 0 1.5 0"/></svg>`;
-        if (type === 'info') {
-            iconSvg = `<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20"><path fill="#3b82f6" d="M10 2a8 8 0 1 0 0 16a8 8 0 0 0 0-16M9 5h2v2H9zm0 4h2v6H9z"/></svg>`;
-        }
+        const iconSvg = `<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20"><path fill="#ffd500" d="M8.681 2.785c.568-1.047 2.071-1.047 2.638 0l6.5 12.002A1.5 1.5 0 0 1 16.502 17H3.498a1.5 1.5 0 0 1-1.319-2.215zM10.5 7.5a.5.5 0 0 0-1 0v4a.5.5 0 0 0 1 0zm.25 6.25a.75.75 0 1 0-1.5 0a.75.75 0 0 0 1.5 0"/></svg>`;
 
         modal.innerHTML = `
             <div class="modal">
@@ -109,18 +106,7 @@ export class ModalManager {
     }
 
     handleAction(action) {
-        const result = {
-            confirmed: action === 'confirm',
-            value: null
-        };
-
-        if (this.activeModal) {
-            const input = this.activeModal.querySelector('#modal-input');
-            if (input) {
-                result.value = input.value;
-            }
-        }
-
+        const result = action === 'confirm';
         this.close();
         if (this.resolvePromise) {
             this.resolvePromise(result);
